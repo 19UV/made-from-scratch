c5917c73f74bbcdfc6ffe7323101a8110b954053
Trevor Martin <the19UV@gmail.com>
2024-04-20 20:21:29 -0400

    Initial Commit

---
+++ src/win32/entry.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+int main(int argc, const char* argv[]) {
+	(void) argc, (void) argv;
+
+	puts("Hello World!");
+
+	return 0;
+}
+
